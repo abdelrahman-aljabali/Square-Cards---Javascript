a20b1f404bdb4bf773388f9b8e96d08126b9573a
Abdelrahman Aljabali <abdelrahmanaljabali1@gmail.com>
2023-09-24 15:32:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+document.getElementsByClassName("bx bxl-youtube")[0].addEventListener("click", 
+function(){
+    window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=20")
+
+})
+
+document.getElementsByClassName("bx bxl-youtube")[1].addEventListener("click", 
+function(){
+    window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=20")
+
+})
+
+document.getElementsByClassName("bx bxl-youtube")[2].addEventListener("click", 
+function(){
+    window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=20")
+
+})
+
+document.getElementsByClassName("bx bxl-youtube")[3].addEventListener("click", 
+function(){
+    window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=20")
+
+})
+
+document.getElementsByClassName("bx bxl-youtube")[4].addEventListener("click", 
+function(){
+    window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=20")
+
+})
+
+document.getElementsByClassName("bx bxl-youtube")[5].addEventListener("click", 
+function(){
+    window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=20")
+
+})
